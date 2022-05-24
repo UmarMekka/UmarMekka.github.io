714fc8507813b9df9cfc5067b4994e14c5eed4f5
UmarMekka <105982186+UmarMekka@users.noreply.github.com>
2022-05-23 22:04:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const hamburger = document.querySelector('#hamburger')
 const links = document.querySelector('.links')
+const linkButtons = document.querySelectorAll('.links button')
+
+links.classList.add('unexpand')
+linkButtons.forEach(e => {
+    e.classList.toggle('disappear');
+})
 
 hamburger.addEventListener('click', () => {
-    console.log('clicked')
-    links.classList.toggle('show');
+    if (links.classList.contains('show')) {
+        linkButtons.forEach(e => {
+        e.classList.toggle('disappear');
+        console.log('poop')
+        })
+        links.classList.toggle('unexpand');
+        setTimeout(() => {
+            links.classList.toggle('show')
+        },
+        "500")
+    } else {
+        linkButtons.forEach(e => {
+            e.classList.toggle('disappear');
+            })
+        links.classList.toggle('unexpand');
+        links.classList.toggle('show');
+        
+    }
 })
