c4779418df6c0d526748414eaa554d4fd7023eb1
UmarMekka <105982186+UmarMekka@users.noreply.github.com>
2022-07-17 20:21:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function sleep(milliseconds) {
     } while (currentDate - date < milliseconds);
   }
 
-function type() {
+(function type() {
     if (count === texts.length){
         count = 0;
     }
@@ -76,9 +76,7 @@ function type() {
       }
     }
 
-    document.querySelector('.typing').innerText = letter;
+    document.querySelector('.typing').textContent = letter;
 
     setTimeout(type, 100);
-};
-
-type();
+}());
